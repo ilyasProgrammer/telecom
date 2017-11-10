78ebb2d6ec3cc223ff422a02694764358381f676
ilyasprogrammer <rakhimkulov@it-projects.info>
2017-11-10 14:10:14 +0500

    separate import for billings and payments

--- telecom_billing/models/billing.py
+++ telecom_billing/models/billing.py
@@ -1,23 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from os.path import expanduser
 import csv
 from StringIO import StringIO
 from datetime import datetime
 from odoo import api, fields, models, _
-import werkzeug
-import cgi
-import json
-from odoo import http
-from odoo.http import request
-from odoo.addons.base_import.controllers.main import ImportController
-from odoo.exceptions import Warning
-from odoo.http import Response
-import itertools
-import operator
 import logging
 
 _logger = logging.getLogger('# ' + __name__)
+PRODUCT_NAME = 'Monthly Telecom & Data Bill'
 
 
 class TelecomBilling(models.Model):
@@ -37,36 +27,50 @@ class TelecomBilling(models.Model):
     ]
 
     @api.one
-    def import_billings(self, content):
-        res = []
-        imported_cnt = 0
+    def import_register_payments(self, spamreader):
         paid_cnt = 0
-        invoice = self.env['account.invoice']
-        payment = self.env['account.payment']
+        invoice_model = self.env['account.invoice']
+        partner_model = self.env['res.partner']
+        payment_model = self.env['account.payment']
+        journal_id = self.env['account.journal'].search([('code', '=', 'BNK1')])
         payment_method_manual_in = self.env.ref("account.account_payment_method_manual_in")
         register_payments_model = self.env['account.register.payments']
+        for row in spamreader:
+            partner_id = partner_model.search([('x_account_no', '=', row['Account Number'])])
+            invoice_ids = invoice_model.search([('partner_id', '=', partner_id.id), ('state', '=', 'draft')])
+            date = datetime.strptime(row['Date'].strip(), '%d/%m/%Y')
+            for inv in invoice_ids:
+                inv.date_invoice = date  # set payment date
+                invoice_ids.action_invoice_open()
+                _logger.info("Validated invoice: %s", inv.invoice_ref)
+            ctx = {'active_model': 'account.invoice', 'active_ids': invoice_ids.ids}
+            register_payments = register_payments_model.with_context(ctx).create({
+                'payment_date': date,
+                'journal_id': journal_id.id,
+                'payment_method_id': payment_method_manual_in.id,
+            })
+            register_payments.create_payment()
+            _logger.info("Payment registered for invoices: %s", invoice_ids.ids)
+            payments = payment_model.search([], order="id desc", limit=1)
+            liquidity_aml = payments.move_line_ids.filtered(lambda r: r.user_type_id.type == 'liquidity')
+            bank_statement = self.reconcile(liquidity_aml, date, partner_id, liquidity_aml.debit, 0, False)
+            _logger.info("Bank statement registered for invoices: %s", invoice_ids.ids)
+            paid_cnt += 1
+
+    @api.one
+    def import_billings(self, content):
+        imported_cnt = 0
+        invoice = self.env['account.invoice']
         line_account = self.env['account.account'].search([('name', '=', 'Sales Tax Control Account')])
         account = self.env['account.account'].search([('name', '=', 'Debtors Control Account')])
         tax = self.env['account.tax'].search([('name', '=', 'S')])[0]
-        journal = self.env['account.journal'].search([('code', '=', 'BNK1')])
-        # filename = '/home/r/1.csv'
-        errors = self.check_rows(content)
-        if len(errors):
-            return {'errors': errors, 'imported_cnt': imported_cnt, 'paid_cnt': paid_cnt}
-        spamreader = csv.reader(StringIO(content), delimiter=',', quotechar='"')
-        for ind, row in enumerate(spamreader):
+        for ind, row in enumerate(reader):
             if ind == 0:
                 continue
             partner_name = row[1].strip()
             partner = self.env['res.partner'].search([('name', '=', partner_name)])
-            product = self.env['product.product'].search([('name', '=', 'Monthly Telecom & Data Bill')])
+            product = self.env['product.product'].search([('name', '=', PRODUCT_NAME)])
             date = fields.Datetime.now()
-            if len(row[11]):
-                try:
-                    date = datetime.strptime(row[11].strip(), '%d/%m/%Y')
-                except:
-                    msg = "Wrong date format in row: %s content: %s" % (ind, row[11])
-                    _logger.info(msg)
             vals = {'partner_id': partner.id,
                     'invoice_ref': row[0].strip(),
                     'payment_method': row[3].strip(),
@@ -75,74 +79,62 @@ class TelecomBilling(models.Model):
                     'date_invoice': date,
                     'is_billing': True,
                     }
-            found = invoice.search([('invoice_ref', '=', row[0].strip())], limit=1)
-            if len(found):
-                msg = "Found existing old billing records: %s" % found.invoice_ref
-                _logger.info(msg)
-                if not row[10].strip() == 'Success' or found.state == 'paid':
-                    msg = "Old invoice not intended to proceed payment and skipped: %s" % found.invoice_ref
-                    _logger.info(msg)
-                    continue
-                else:
-                    new_invoice = found
-                    _logger.info("Old invoice going to be paid: %s", row[0].strip())
-            else:
-                new_invoice = invoice.create(vals)
-                imported_cnt += 1
-                _logger.info("New invoice created: %s", row[0].strip())
-                amount = float(row[6].strip())
-                line_vals = {'product_id': product.id,
-                             'account_id': line_account.id,
-                             'price_unit': amount,
-                             'name': 'imported',
-                             'partner_id': partner.id,
-                             'invoice_line_tax_ids': [(6, 0, [tax.id])],
-                             }
-                new_invoice.write({'invoice_line_ids': [(0, 0, line_vals)]})
-                new_invoice.compute_taxes()
-            # Register payment
-            if int(row[10].strip() == 'Success'):
-                new_invoice.date_invoice = date  # update date before
-                new_invoice.action_invoice_open()
-                _logger.info("Validated invoice: %s", new_invoice.invoice_ref)
-                ctx = {'active_model': 'account.invoice', 'active_ids': [new_invoice.id]}
-                register_payments = register_payments_model.with_context(ctx).create({
-                    'payment_date': date,
-                    'journal_id': journal.id,
-                    'payment_method_id': payment_method_manual_in.id,
-                })
-                register_payments.create_payment()
-                _logger.info("Payment registered for invoice: %s", new_invoice.invoice_ref)
-                payments = payment.search([], order="id desc", limit=1)
-                liquidity_aml = payments.move_line_ids.filtered(lambda r: r.user_type_id.type == 'liquidity')
-                bank_statement = self.reconcile(liquidity_aml, date, partner, liquidity_aml.debit, 0, False)
-                _logger.info("Bank statement registered for invoice: %s", new_invoice.invoice_ref)
-                paid_cnt += 1
-        return {'errors': res, 'imported_cnt': imported_cnt, 'paid_cnt': paid_cnt}
+            new_invoice = invoice.create(vals)
+            imported_cnt += 1
+            _logger.info("New invoice created: %s", row[0].strip())
+            amount = float(row[6].strip())
+            line_vals = {'product_id': product.id,
+                         'account_id': line_account.id,
+                         'price_unit': amount,
+                         'name': 'imported',
+                         'partner_id': partner.id,
+                         'invoice_line_tax_ids': [(6, 0, [tax.id])],
+                         }
+            new_invoice.write({'invoice_line_ids': [(0, 0, line_vals)]})
+            new_invoice.compute_taxes()
 
-    def check_rows(self, rows):
-        r1 = csv.reader(StringIO(rows), delimiter=',', quotechar='"')
+    @api.one
+    def import_file(self, content):
+        errors = []
+        imported_cnt = 0
+        paid_cnt = 0
+        reader = csv.DictReader(StringIO(content), delimiter=',', quotechar='"')
+        ln = len(reader.fieldnames)
+        if ln == 4:  # importing file for payment register
+            errors = self.check_payments_rows(content)
+            if len(errors) == 0:
+                paid_cnt = self.import_register_payments(content)
+        elif ln == 10:  # importing billings without payments
+            errors = self.check_import_rows(content)
+            if len(errors) == 0:
+                imported_cnt = self.import_billings(content)
+        else:
+            errors.append('Wrong amount of columns in file. Expected 4 or 10. Got %s' % ln)
+        return {'errors': errors, 'imported_cnt': imported_cnt, 'paid_cnt': paid_cnt}
+
+    def check_import_rows(self, content):
         res = []
         ref_numbers = set()
         duplicated_refs = []
-        for row in r1:
+        invoice_model = self.env['account.invoice']
+        reader = csv.DictReader(StringIO(content), delimiter=',', quotechar='"')
+        for row in reader:
             if row[0] not in ref_numbers:
                 ref_numbers.add(row[0])
             else:
                 duplicated_refs.append(row[0])
         if len(duplicated_refs):
             res.append('duplicated refs:' + str(duplicated_refs))
-        invoice = self.env['account.invoice']
-        r2 = csv.reader(StringIO(rows), delimiter=',', quotechar='"')
-        for ind, row in enumerate(r2):
+        reader = csv.DictReader(StringIO(content), delimiter=',', quotechar='"')
+        for ind, row in enumerate(reader):
             if ind == 0:
                 continue
-            # found = invoice.search([('invoice_ref', '=', row[0].strip())], limit=1)
-            # if len(found):
-            #     msg = "Found existing old billing records: %s" % found.invoice_ref
-            #     _logger.error(msg)
-            #     res.append(msg)
-            #     return res
+            found = invoice_model.search([('invoice_ref', '=', row[0].strip())], limit=1)
+            if len(found):
+                msg = "Found existing old billing records: %s" % found.invoice_ref
+                _logger.error(msg)
+                res.append(msg)
+                return res
             partner_name = row[1].strip()
             partner = self.env['res.partner'].search([('name', '=', partner_name)])
             if not partner:
@@ -155,9 +147,97 @@ class TelecomBilling(models.Model):
                 _logger.error(msg)
                 res.append(msg)
                 return res
-            product = self.env['product.product'].search([('name', '=', 'Electricity Bill')])
+            product = self.env['product.product'].search([('name', '=', PRODUCT_NAME)])
             if not product:
-                msg = "Product not found: %s" % 'Electricity Bill'
+                msg = "Product not found: %s" % PRODUCT_NAME
+                _logger.error(msg)
+                res.append(msg)
+                return res
+            if len(row['Date']):
+                try:
+                    date = datetime.strptime(row['Date'].strip(), '%d/%m/%Y')
+                except:
+                    msg = "Wrong date format in row: %s" % row
+                    _logger.info(msg)
+            else:
+                msg = "Date is absent in row: %s" % row
+                _logger.error(msg)
+                res.append(msg)
+                return res
+        return res
+
+    def check_payments_rows(self, content):
+        res = []
+        ref_numbers = set()
+        duplicated_refs = []
+        invoice_model = self.env['account.invoice']
+        reader = csv.DictReader(StringIO(content), delimiter=',', quotechar='"')
+        for row in reader:
+            if row['Account Number'] not in ref_numbers:
+                ref_numbers.add(row['Account Number'])
+            else:
+                duplicated_refs.append(row['Account Number'])
+        if len(duplicated_refs):
+            res.append('Duplicated Account Number:' + str(duplicated_refs))
+        reader = csv.DictReader(StringIO(content), delimiter=',', quotechar='"')
+        for row in reader:
+            if not len(row['Account Number']):
+                msg = "Empty Account Number in row: %s" % row
+                _logger.error(msg)
+                res.append(msg)
+                return res
+            x_account_no = row['Account Number'].strip()
+            partner = self.env['res.partner'].search([('x_account_no', '=', x_account_no)])
+            if not partner:
+                msg = "Partner not found: %s" % x_account_no
+                _logger.error(msg)
+                res.append(msg)
+                return res
+            if len(partner) > 1:
+                msg = "More than one partner with same x_account_no: %s" % x_account_no
+                _logger.error(msg)
+                res.append(msg)
+                return res
+            invoice_ids = invoice_model.search([('partner_id', '=', partner.id), ('state', '=', 'draft')])
+            if len(invoice_ids) == 0:
+                msg = "There is no draft invoices for partner %s in line %s" % (partner.name, row)
+                _logger.error(msg)
+                res.append(msg)
+                return res
+            product = self.env['product.product'].search([('name', '=', PRODUCT_NAME)])
+            if not product:
+                msg = "Product not found: %s" % PRODUCT_NAME
+                _logger.error(msg)
+                res.append(msg)
+                return res
+            if len(row['Total']):
+                if float(row['Total']) <= 0:
+                    msg = "Total is less that 0 for x_account_no: %s" % x_account_no
+                    _logger.error(msg)
+                    res.append(msg)
+                    return res
+            else:
+                msg = "Total is absent for x_account_no: %s" % x_account_no
+                _logger.error(msg)
+                res.append(msg)
+                return res
+            if len(row['Date']):
+                try:
+                    date = datetime.strptime(row['Date'].strip(), '%d/%m/%Y')
+                except:
+                    msg = "Wrong date format for x_account_no: %s" % x_account_no
+                    _logger.info(msg)
+            else:
+                msg = "Date is absent for x_account_no: %s" % x_account_no
+                _logger.error(msg)
+                res.append(msg)
+                return res
+            if len(row['Paid']):
+                if row['Paid'] != 'Success':
+                    msg = "No Success for x_account_no: %s" % x_account_no
+                    _logger.info(msg)
+            else:
+                msg = "Empty Paid field for x_account_no: %s" % x_account_no
                 _logger.error(msg)
                 res.append(msg)
                 return res
@@ -199,7 +279,7 @@ class Extension(models.TransientModel):
         report = ''
         if self.res_model != 'account.invoice':
             return super(Extension, self).parse_preview(options, count)
-        res = self.env['account.invoice'].search([])[0].import_billings(self.file)[0]
+        res = self.env['account.invoice'].search([])[0].import_file(self.file)[0]
         if len(res['errors']) == 0:
             report += "Everything ok \n"
             report += "Imported invoices: %s \n" % res['imported_cnt']
